c9a5f6c5368fa52f03e13797588ee91edd3ee09d
ulaskarakas <ulaskarakas95@gmail.com>
2025-07-27 19:46:40 +0300

    Create UnitOfWork pattern for transactions

---
+++ StudentInfoSys.Data/UnitOfWork/IUnitOfWork.cs
@@ -0,0 +1,10 @@
+﻿namespace StudentInfoSys.Data.UnitOfWork
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        Task<int> SaveChangesAsync();
+        Task BeginTransaction();
+        Task CommitTransaction();
+        Task RollbackTransaction();
+    }
+}
---
+++ StudentInfoSys.Data/UnitOfWork/UnitOfWork.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore.Storage;
+using StudentInfoSys.Data.Context;
+
+namespace StudentInfoSys.Data.UnitOfWork
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly StudentInfoSysDbContext _db;
+        private IDbContextTransaction? _transaction;
+
+        public UnitOfWork(StudentInfoSysDbContext db)
+        {
+            _db = db;
+        }
+
+        public async Task BeginTransaction()
+        {
+            _transaction = await _db.Database.BeginTransactionAsync();
+        }
+
+        public async Task CommitTransaction()
+        {
+            if (_transaction != null)
+            {
+                await _transaction.CommitAsync();
+                await _transaction.DisposeAsync();
+            }
+        }
+
+        public async Task RollbackTransaction()
+        {
+            if (_transaction != null)
+            {
+                await _transaction.RollbackAsync();
+                await _transaction.DisposeAsync();
+            }
+        }
+
+        public async Task<int> SaveChangesAsync()
+        {
+            return await _db.SaveChangesAsync();
+        }
+
+        public void Dispose()
+        {
+            _db.Dispose(); // Cleaning permission granted to Garbage Collector
+        }
+    }
+}
